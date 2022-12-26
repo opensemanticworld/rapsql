18dd5e801601ad6f81c14e7451e7c9db9b7bc221
Andreas Raeder <andreas.raeder@web.de>
2022-12-26 15:40:35 +0000

    merge upd from dev

--- dev/sql/rap.sql
+++ dev/sql/rap.sql
@@ -2,18 +2,94 @@
 
 /*  Function api.rapsql_query(querystring VARCHAR)
     Author(s):  Andreas Raeder
-    Version:    0.1.1
-    Descript.:  Postgrest RPC for json return by using dynamic apache age cypher query string
-    Hint(s):    No permission to "LOAD 'age';" for REST API yet
-                Can return multiple JSON results of one attribute yet
-    ToDo(s):    Specifiy permissions
-                Implementation of multiple attributes including nested JSON object refered to agtype return
-    Usage:      POST request e. g. on swagger ui, example JSON body          
-                    {
-                        "querystring": "SELECT * FROM cypher('countries', $$ MATCH (e) RETURN properties(e) $$) AS (properties agtype) LIMIT 20;"
-                    }
+    Version:    0.1.3
+    Descript.:  
+                Postgrest RPC for json return by using dynamic apache age cypher query string; 
+                Can return multiple JSON results of multiple attributes (experimental) 
+    ToDo(s):    
+                Testing different edge cases
+                Maybe improvement by type cast
+                Check responses with "::vertex, ::edge, ::path" ending
+    Usage:      
+                {
+                    "querystring": "SELECT * FROM cypher('countries', $$ MATCH (e) RETURN properties(e) $$) AS (properties agtype) LIMIT 3"
+                }
+                {
+                    "querystring": "SELECT * FROM cypher('countries', $$ MATCH (country {name: 'Germany'})<-[r]-(city) RETURN properties(country), city.name $$) as (country agtype, city agtype) LIMIT 3"
+                }
+    
     */
+    DROP FUNCTION IF EXISTS api.rapsql_query(querystring VARCHAR);
+
     CREATE OR REPLACE FUNCTION api.rapsql_query(querystring VARCHAR)
+    RETURNS SETOF JSON
+    LANGUAGE plpgsql
+    AS $function$
+    DECLARE 
+        sql_agtype VARCHAR;
+    BEGIN
+        SET search_path TO ag_catalog;
+        sql_agtype := format('SELECT (to_json(replace(replace(replace(replace((row_to_json(t))::text, ''\"'', ''"''), ''"{'', ''{''), ''}"'', ''}''), ''""'', ''"'')) #>> ''{}'')::JSON FROM (%s) t;', querystring);
+        RETURN QUERY EXECUTE sql_agtype;
+    END
+    $function$;
+    /* End: Function api.rapsql_query(querystring VARCHAR) 
+*/
+
+/*  Function api.rapsql_query2(querystring VARCHAR)
+    Author(s):  Andreas Raeder
+    Version:    0.1.2
+    Descript.:  
+                Postgrest RPC for json return by using dynamic apache age cypher query string; 
+                Can return multiple JSON results of multiple attributes, but cypher response is escaped
+    Usage:      
+                {
+                    "querystring": "SELECT * FROM cypher('countries', $$ MATCH (e) RETURN properties(e) $$) AS (properties agtype) LIMIT 3"
+                }
+                {
+                    "querystring": "SELECT * FROM cypher('countries', $$ MATCH (country {name: 'Germany'})<-[r]-(city) RETURN properties(country), city.name $$) as (countries agtype, cities agtype) LIMIT 3"
+                }
+    
+    */
+    DROP FUNCTION IF EXISTS api.rapsql_query2(querystring VARCHAR);
+
+    CREATE OR REPLACE FUNCTION api.rapsql_query2(querystring VARCHAR)
+    RETURNS SETOF JSON
+    LANGUAGE plpgsql
+    AS $function$
+    DECLARE 
+        sql_agtype VARCHAR;
+        sql_agtype_output ag_catalog.agtype;
+        _result JSON;
+    BEGIN
+        SET search_path TO ag_catalog;
+        sql_agtype := format('SELECT row_to_json((t.*) FROM (%s)  t;', querystring);
+        RETURN QUERY EXECUTE sql_agtype;
+    END
+    $function$;
+    /* End: Function api.rapsql_query2(querystring VARCHAR) 
+*/
+
+/*  Function api.rapsql_query1(querystring VARCHAR)
+    Author(s):  Andreas Raeder
+    Version:    0.1.1
+    Descript.:  
+                Postgrest RPC for json return by using dynamic apache age cypher query string; 
+                Can return multiple JSON results of one attribute yet
+    Hint(s):    
+                No permission to "LOAD 'age';" for REST API yet
+    ToDo(s):    
+                Specifiy permissions
+                Implementation of multiple attributes including nested JSON object refered to agtype return
+    Usage:      
+                POST request e. g. on swagger ui, example JSON body          
+                {
+                    "querystring": "SELECT * FROM cypher('countries', $$ MATCH (e) RETURN properties(e) $$) AS (properties agtype) LIMIT 20;"
+                }
+    */
+    DROP FUNCTION IF EXISTS api.rapsql_query1(querystring VARCHAR);        
+
+    CREATE OR REPLACE FUNCTION api.rapsql_query1(querystring VARCHAR)
     RETURNS SETOF JSON
     LANGUAGE plpgsql
     AS $function$
@@ -33,6 +109,6 @@
         RETURN;
     END
     $function$;
-    /* End: Function api.rapsql_query(querystring VARCHAR) 
+    /* End: Function api.rapsql_query1(querystring VARCHAR) 
 */
 
