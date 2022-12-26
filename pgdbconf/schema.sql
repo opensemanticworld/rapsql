74405b12731c920685a542b48be75673050eb1a8
Andreas Raeder <andreas.raeder@web.de>
2022-12-26 16:03:16 +0000

    merge from dev

--- pgdbconf/schema.sql
+++ pgdbconf/schema.sql
@@ -95,7 +95,7 @@
 /*  Function api.rapsql_query(querystring VARCHAR)
     Author(s):  Andreas Raeder
     Source(s):  https://github.com/OpenSemanticLab/rapsql/blob/main/dev/sql/rap.sql 
-    Version:    0.1.1
+    Version:    0.1.3
     */
     CREATE OR REPLACE FUNCTION api.rapsql_query(querystring VARCHAR)
     RETURNS SETOF JSON
@@ -103,19 +103,12 @@
     AS $function$
     DECLARE 
         sql_agtype VARCHAR;
-        sql_agtype_output ag_catalog.agtype;
-        _result JSON;
     BEGIN
-        -- LOAD 'age'; 
         SET search_path TO ag_catalog;
-        sql_agtype := format('%s', querystring);
-        FOR sql_agtype_output IN EXECUTE sql_agtype 
-        LOOP 
-            SELECT (to_json(sql_agtype_output) #>> '{}')::JSON INTO _result;
-            RETURN NEXT _result;
-        END LOOP;
-        RETURN;
+        sql_agtype := format('SELECT (to_json(replace(replace(replace(replace((row_to_json(t))::text, ''\"'', ''"''), ''"{'', ''{''), ''}"'', ''}''), ''""'', ''"'')) #>> ''{}'')::JSON FROM (%s) t;', querystring);
+        RETURN QUERY EXECUTE sql_agtype;
     END
     $function$;
     /* End: Function api.rapsql_query(querystring VARCHAR) 
 */
+
